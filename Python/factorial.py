07c2ea900ed5b454ad00a738b2910c95fb5015e7
harshit257 <55016133+harshit257@users.noreply.github.com>
2020-10-03 13:18:21 +0530

    Update factorial.py
    recursion using for loop

--- Python/factorial.py
+++ Python/factorial.py
@@ -25,6 +25,24 @@ def fact(n):
         return 1
     elif n >= 1:
         return n * fact(n-1)
+    #using for loop
+    num = 7
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+
         
 
 print(factorial(5))
